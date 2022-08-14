6fd119f47d5badc90a29fa978586259ba96f996f
EggcellentCode <dankim440@gmail.com>
2022-08-14 15:59:47 -0400

    Finished routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 module.exports = app; // this line is only used to make testing easier.
 
 // remember to plug in your router and any other middleware you may need here (i.e. body parser, mounting any router-level middleware, etc.)
+app.use(express.json());
+app.use('/users', routes);
 
 app.use((err, req, res, next) => {
   res.sendStatus(err.status);
--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,55 @@ const todos = require('../models/express-models/todos');
 module.exports = router;
 
 // write your routes here. Feel free to split into multiple files if you like.
+router.get("/", async (req, res, next) => {
+    try {
+        const users = await todos.listPeople();
+        res.json(users);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:name/tasks', async (req, res, next) => {
+    try {
+        if (!todos.listPeople().includes(req.params.name)) {
+            res.status(404).send("Not Found");
+          } else {
+            res.send(todos.list(req.params.name, req.body))
+          }
+        } catch (error) {
+          next(error);
+        }
+      
+});
+
+router.post("/:name/tasks", async (req, res, next) => {
+    try {
+        if (!req.body.content) {
+            res.status(400).send("Does not Exist");
+        } else {
+            todos.add(req.params.name, req.body);
+            res.status(201).send(todos.list(req.params.name)[0]);
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/:name/tasks/:index', (req, res, next) => {
+    try {
+      todos.complete(req.params.name, req.params.index);
+      res.sendStatus(200);
+    } catch (error) {
+      next(error);
+    }
+  })
+  
+  router.delete('/:name/tasks/:index', (req, res, next) => {
+    try {
+      todos.remove(req.params.name, req.params.index);
+      res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
+  })
