80839b3b62c1830c4cec78cefef0489b272f777a
EggcellentCode <dankim440@gmail.com>
2022-08-14 01:30:53 -0400

    Finished Todos

--- models/express-models/todos.js
+++ models/express-models/todos.js
@@ -18,21 +18,36 @@ module.exports = {
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    return Object.keys(tasks);
   },
 
   add: function (name, task) {
     // saves a task for a given person
+    if (tasks.hasOwnProperty(name)) {
+      tasks[name].push(task);
+    } else {
+      tasks[name] = [task];
+    }
+    if (!task.complete) {
+      task.complete = false;
+    } else {
+      task.complete = true;
+    }
   },
 
   list: function (name) {
     // returns tasks for specified person
+    return tasks[name];
   },
 
   complete: function (name, idx) {
     // marks a task complete
+      tasks[name][idx].complete = true;
   },
+
 
   remove: function (name, idx) {
     // removes a tasks
+    tasks[name].splice(idx, 1);
   },
 };
